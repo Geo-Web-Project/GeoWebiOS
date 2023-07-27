4e77c0e0a6d448c081cbc769562ff39a2491fa7d
Cody Hatfield <cody.hatfield@me.com>
2023-07-27 13:54:22 -0700

    Initial Commit

---
+++ Hubble/HubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubbleApp.swift
+//  Hubble
+//
+//  Created by Cody Hatfield on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct HubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
